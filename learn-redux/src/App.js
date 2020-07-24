ab91aad86827e25eaf1c39a3c54e3f6e36ce69ea
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2020-07-23 21:29:01 -0400

    Provider import error, breaking rules of hooks

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLoggedIn = useSelector(state => state.isLogged);
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter {counter} </h1>
+      {isLoggedIn ? <h3>This info should be hidden</h3> : ''}
     </div>
   );
 }
--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -6,10 +6,13 @@ import { createStore } from 'redux';
 import allReducer from './reducers/reducers';
 import { Provider } from 'react-redux';
 
-const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const myStore = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <Provider store={store} >
+  <Provider store={myStore}>
     <App />
   </Provider>,
 
